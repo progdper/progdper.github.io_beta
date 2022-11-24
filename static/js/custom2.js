a5f6eb3ebff36774fe63cc9cb7c51b412cf5829a
최재훈 <94341515+progdper@users.noreply.github.com>
2022-11-24 23:27:22 +0900

    inedx change
    내용 수정

---
+++ static/js/custom2.js
@@ -0,0 +1,42 @@
+(function() {
+	'use strict';
+
+	
+
+
+	var portfolioMasonry = function() {
+ $('.filters ul li').click(function(){
+        $('.filters ul li').removeClass('active');
+        $(this).addClass('active');
+        
+        var data = $(this).attr('data-filter');
+        $grid.isotope({
+          filter: data
+        })
+      });
+
+
+      if(document.getElementById("section-portfolio")){
+            var $grid = $(".grid").isotope({
+              itemSelector: ".all",
+              percentPosition: true,
+              masonry: {
+                columnWidth: ".all"
+              }
+            })
+      };
+
+
+	};
+
+
+	$(function(){
+
+		portfolioMasonry();
+	});
+
+	
+
+
+})();
+
